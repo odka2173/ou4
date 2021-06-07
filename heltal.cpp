ea9fe27b448bdb3c5210fa5f917fc60fc2b12076
odka2173 <85465137+odka2173@users.noreply.github.com>
2021-06-07 03:41:06 +0200

    Update heltal.cpp

--- heltal.cpp
+++ heltal.cpp
@@ -21,12 +21,28 @@ int Heltal::get(){
 void Heltal::set(int n){
 	val = n;
 	}
+
+void Heltal::fib(int n){
+    if(n==0)
+    {
+        return 0;
+    }
+    else if(n==1)
+    {
+        return 1;
+    }
+    else
+    {
+        return fib(n-1) + fib(n-2);
+    }
+}
 	
 
 extern "C"{
 	Heltal* Heltal_new(int n) {return new Heltal(n);}
 	int Heltal_get(Heltal* heltal) {return heltal->get();}
 	void Heltal_set(Heltal* heltal, int n) {heltal->set(n);}
+	void Heltal_fib(Heltal* heltal, int n) {return heltal->fib(n);}
 	void Heltal_delete(Heltal* heltal){
 		if (heltal){
 			delete heltal;
